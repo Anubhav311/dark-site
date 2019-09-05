04f295403412e1ade10bbc49139bddb4dd4c4fca
Anubhav311 <anubhavbhambri1@gmail.com>
2019-09-05 19:12:18 +0530

    created raw layout

---
+++ components/Layout.js
@@ -0,0 +1,95 @@
+const Layout = ({ children }) => (
+    <div className='container-div'>
+
+    <div className='inner-div'>
+        <h2><span style={{color: '#E6296D'}}>Anubhav</span> Bhambri</h2>
+        <nav>
+            <ul>
+                <li><a href="">ABOUT</a></li>
+                <li><a href="">TECHNOLOGIES</a></li>
+                <li><a href="">PROJECTS</a></li>
+            </ul>  
+        </nav>
+        <p className='contact'>CONTACT</p>
+    </div>
+
+    <style jsx>{`
+    .test {
+        float: left;
+        display: block;
+    }
+    .container-div {
+        background-color: #4B4B4B;
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+    }
+    .inner-div {
+        width: 85%;
+        height: 80%;
+        background-color: #333333;
+        margin: auto;
+        box-shadow: 0px 15px 40px 10px;
+        padding: 20px;
+        position: relative;
+    }
+    .inner-div h2 {
+        margin: 0;
+    }
+    nav{
+        margin-top: 30px;
+        transform-origin: left top;
+        transform: rotate(-90deg) translateX(-100%);  
+        width: 65vh;
+        height: auto;
+        // background-color: white;
+    }      
+    ul{
+        display:flex;
+        flex-flow: row wrap;
+        align-items: center;
+    }
+    li{
+        // border: 1px solid;
+        
+        height: auto;
+        list-style: none;
+        color: white;
+        text-align: center;
+        transition: background .5s;
+        
+        display: flex;
+        align-items: center;
+        flex: 1;
+    }
+    a{  
+        flex:1;
+        color:#F4F1F3;
+        transition: color .5s;
+        display: block;
+        text-decoration: none;
+    }    
+    a:hover{ 
+        background: black;
+        color:white;
+    }      
+    .contact {
+        margin: 0;
+        position: absolute;
+        bottom: 20px;
+        right: 20px;
+    }
+    `}</style>
+    <style global jsx>{`
+    body {
+        margin: 0;
+        padding: 0;
+    }
+    h1, h2, h3, h4, h5, h6, p, ul, li {
+        color: #F4F1F3;
+    }
+    `}</style>
+</div>
+)
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,8 @@
 import React from 'react'
+import Layout from '../components/Layout';
 
 const Home = () => (
-  <div className='container-div'>
-
-    <div className='inner-div'>
-
-    </div>
-
-    <style jsx>{`
-      .container-div {
-        background-color: #4B4B4B;
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-      }
-      .inner-div {
-        width: 85%;
-        height: 80%;
-        background-color: #333333;
-        margin: auto;
-        box-shadow: 0px 15px 40px 10px;
-      }
-    `}</style>
-    <style global jsx>{`
-      body {
-          margin: 0;
-          padding: 0;
-      }
-    `}</style>
-  </div>
+  <Layout/>
 )
 
 export default Home
