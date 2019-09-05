a5cc4cbbfa9e4db7d55af368402b4f4f0f387310
Anubhav311 <anubhavbhambri1@gmail.com>
2019-09-05 17:35:04 +0530

    added color to base divs

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Home = () => (
   <div className='container-div'>
+
     <div className='inner-div'>
 
     </div>
 
     <style jsx>{`
       .container-div {
-        background-color: black;
+        background-color: #4B4B4B;
         width: 100vw;
         height: 100vh;
         display: flex;
@@ -16,8 +17,9 @@ const Home = () => (
       .inner-div {
         width: 85%;
         height: 80%;
-        background-color: red;
+        background-color: #333333;
         margin: auto;
+        box-shadow: 0px 15px 40px 10px;
       }
     `}</style>
     <style global jsx>{`
