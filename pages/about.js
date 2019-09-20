489e39b8e5faa537fc28202405076ce130d57f72
Anubhav311 <anubhavbhambri1@gmail.com>
2019-09-20 23:45:35 +0530

    changed text on about.js page

--- pages/about.js
+++ pages/about.js
@@ -6,11 +6,10 @@ const About = () => (
         <div className="about-container">
             <div className="about-inner-container">
                 <h1>ABOUT</h1>
-                <p>I like teaching</p>
-                <p>I used to work as a corporate lawyer</p>
-                <p>I'm a Lambda School graduate</p>
-                <p>I like reading books, startups, swimming and table tennis</p>
-                <p>I like making friends. So don't hesitate to reach out if you think we can be friends.</p>
+                <p>I'm a student of Full-Stack Web Development and Computer Science at <a href="https://lambdaschool.com/">Lambda School</a></p>
+                <p>Before making switch to Web Development, I worked in Corporate Law industry for three years.</p>
+                <p>I enjoy reading books, learning new technologies, teaching, swimming and table tennis</p>
+                <p>My believe is that startups are the engine behind growth of societies and individuals. And they are so cool that I can't stop thinking about them.</p>
             </div>
         </div>
         <style jsx>{`
