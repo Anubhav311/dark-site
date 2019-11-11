57fb4507f5d54abd37f5d701b65723e806f891cc
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-11 10:57:21 +0530

    created separate file for projects

---
+++ data/projects.js
@@ -0,0 +1,60 @@
+let projects = [
+    {
+        title: 'Where-To-Code',
+        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
+        role: 'I was working as a Team Lead for this project.',
+        tech: 'React || Express || Node || Google Map || Travis',
+        image: '../static/project-images/where-to-code.png',
+    },
+    {
+        title: 'Roadtrip',
+        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
+        role: 'I was working as a Team Lead for this project.',
+        tech: 'React || Express || Node || Google Map || Travis',
+        image: '../static/project-images/roadtrip.png',
+    },
+    {
+        title: 'ChallengeJS',
+        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
+        role: 'I was working as a Team Lead for this project.',
+        tech: 'React || Express || Node || Google Map || Travis',
+        image: '../static/project-images/challengejs.png',
+    },
+    {
+        title: 'Modern Day Researcher',
+        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
+        role: 'I was working as a Team Lead for this project.',
+        tech: 'React || Express || Node || Google Map || Travis',
+        image: '../static/project-images/modern-day-researcher.png',
+    },
+    {
+        title: 'Win Place Show',
+        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
+        role: 'I was working as a Team Lead for this project.',
+        tech: 'React || Express || Node || Google Map || Travis',
+        image: '../static/project-images/winplaceshow.png',
+    },
+    {
+        title: 'Instagram Clone',
+        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
+        role: 'I was working as a Team Lead for this project.',
+        tech: 'React || Express || Node || Google Map || Travis',
+        image: '../static/project-images/insta_clone.png',
+    },
+    {
+        title: 'Todo List',
+        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
+        role: 'I was working as a Team Lead for this project.',
+        tech: 'React || Express || Node || Google Map || Travis',
+        image: '../static/project-images/todo-list.png',
+    },
+    {
+        title: 'Donor Management',
+        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
+        role: 'I was working as a Team Lead for this project.',
+        tech: 'React || Express || Node || Google Map || Travis',
+        image: '../static/project-images/donor_management.png',
+    },
+]
+
+export default projects;
--- pages/projects.js
+++ pages/projects.js
@@ -4,65 +4,7 @@ import Layout from '../components/Layout';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub as github } from '@fortawesome/free-brands-svg-icons';
 import { faExternalLinkAlt as link, faEnvelope as mail } from '@fortawesome/free-solid-svg-icons';
-
-let projects = [
-    {
-        title: 'Where-To-Code',
-        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
-        role: 'I was working as a Team Lead for this project.',
-        tech: 'React || Express || Node || Google Map || Travis',
-        image: '../static/project-images/where-to-code.png',
-    },
-    {
-        title: 'Roadtrip',
-        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
-        role: 'I was working as a Team Lead for this project.',
-        tech: 'React || Express || Node || Google Map || Travis',
-        image: '../static/project-images/roadtrip.png',
-    },
-    {
-        title: 'ChallengeJS',
-        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
-        role: 'I was working as a Team Lead for this project.',
-        tech: 'React || Express || Node || Google Map || Travis',
-        image: '../static/project-images/challengejs.png',
-    },
-    {
-        title: 'Modern Day Researcher',
-        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
-        role: 'I was working as a Team Lead for this project.',
-        tech: 'React || Express || Node || Google Map || Travis',
-        image: '../static/project-images/modern-day-researcher.png',
-    },
-    {
-        title: 'Win Place Show',
-        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
-        role: 'I was working as a Team Lead for this project.',
-        tech: 'React || Express || Node || Google Map || Travis',
-        image: '../static/project-images/winplaceshow.png',
-    },
-    {
-        title: 'Instagram Clone',
-        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
-        role: 'I was working as a Team Lead for this project.',
-        tech: 'React || Express || Node || Google Map || Travis',
-        image: '../static/project-images/insta_clone.png',
-    },
-    {
-        title: 'Todo List',
-        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
-        role: 'I was working as a Team Lead for this project.',
-        tech: 'React || Express || Node || Google Map || Travis',
-        image: '../static/project-images/todo-list.png',
-    },
-    {
-        title: 'Donor Management',
-        description: 'Where to code is an online platform for finding nearby places where user can sit and work remotely.',
-        role: 'I was working as a Team Lead for this project.',
-        tech: 'React || Express || Node || Google Map || Travis',
-        image: '../static/project-images/donor_management.png',
-    },
-]
+import projects from '../data/projects';
 
 const Projects = () => (
     <Layout>
