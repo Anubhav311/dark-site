1b857b5f2117772d0190ee60c4178344abb84d90
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-11 10:41:12 +0530

    created separate file for tech items data

---
+++ data/technologies.js
@@ -0,0 +1,40 @@
+let technologies = [
+    {
+        name: 'JavaScript',
+        logo: '../static/tech-images/javascript.png',
+    },
+    {
+        name: 'Python',
+        logo: '../static/tech-images/python.png',
+    },
+    {
+        name: 'React',
+        logo: '../static/tech-images/react.png',
+    },
+    {
+        name: 'Next',
+        logo: '../static/tech-images/next_black.png',
+    },
+    {
+        name: 'Redux',
+        logo: '../static/tech-images/redux.png',
+    },
+    {
+        name: 'Node',
+        logo: '../static/tech-images/node.png',
+    },
+    {
+        name: 'Express',
+        logo: '../static/tech-images/express.png',
+    },
+    {
+        name: 'Django',
+        logo: '../static/tech-images/django.png',
+    },
+    {
+        name: 'GitHub',
+        logo: '../static/tech-images/github.png',
+    },
+]
+
+export default technologies;
